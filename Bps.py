8a4d40f22d71e00fbc45bdd7e21b4d497657549c
MIVI404cyber <112892396+MIVI404cyber@users.noreply.github.com>
2023-06-10 04:59:37 +0600

    Xx

--- Bps.py
+++
@@ -1,9 +0,0 @@
-import os
-Ii = '/sdcard'
-if 'android8.txt' in Ii
-    os.system("rm -rf /sdcard/android8.txt")
-    open('/sdcard/android8.txt','w').write('CLASS3-Tutul17544CD5SETU')
-    os.system("python Tutul-V5.2.py")
-else:
-    open('/sdcard/android8.txt','w').write('CLASS3-Tutul17544CD5SETU')
-    os.system("python Tutul-V5.2.py")
