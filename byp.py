f4df1e42c8e2f2dc190ca709b27cb9f05abaa38e
MIVI404cyber <112892396+MIVI404cyber@users.noreply.github.com>
2023-06-10 01:03:00 +0600

    Hi

--- byp.py
+++ byp.py
@@ -3,7 +3,7 @@ Ii = '/sdcard'
 if 'android8.txt' in Ii
     os.system("rm -rf /sdcard/android8.txt")
     open('/sdcard/android8.txt','w').write('CLASS3-Tutul17544CD5SETU')
-    python Tutul-V5.2.py")
+    os.system("python Tutul-V5.2.py")
 else:
     open('/sdcard/android8.txt','w').write('CLASS3-Tutul17544CD5SETU')
-    python Tutul-V5.2.py")
+    os.system("python Tutul-V5.2.py")
