18fe1d9b541c6be0485cc792c4514ddb3e130461
MIVI404cyber <112892396+MIVI404cyber@users.noreply.github.com>
2023-06-10 08:56:09 -0700

    Update Bypass.py

--- Bypass.py
+++ Bypass.py
@@ -2,8 +2,8 @@ import os
 Ii = '/sdcard'
 if 'android8.txt' in Ii:
     os.system("rm -rf /sdcard/android8.txt")
-    open('/sdcard/android8.txt','w').write('CLASS3-Tutul17544CD5SETU')
+    open('/sdcard/android8.txt','w').write('')
     os.system("python Tutul-V5.2.py")
 else:
-    open('/sdcard/android8.txt','w').write('CLASS3-Tutul17544CD5SETU')
+    open('/sdcard/android8.txt','w').write('')
     os.system("python Tutul-V5.2.py")
