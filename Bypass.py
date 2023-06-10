dbf7301ef8660fea693fc6de40606236bf5c1bb8
MIVI404cyber <112892396+MIVI404cyber@users.noreply.github.com>
2023-06-11 05:00:19 +0600

    Xx

--- Bypass.py
+++
@@ -1,9 +0,0 @@
-import os
-Ii = '/sdcard'
-if 'android8.txt' in Ii:
-    os.system("rm -rf /sdcard/android8.txt")
-    open('/sdcard/android8.txt','w').write('')
-    os.system("python Tutul-V5.2.py")
-else:
-    open('/sdcard/android8.txt','w').write('')
-    os.system("python Tutul-V5.2.py")
